d269a1f22a8e07fc04f926388726f461b4dccb60
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-23 20:46:20 +0800

    Finished Code!

--- exercise15.py
+++ exercise15.py
@@ -6,3 +6,12 @@
         # Multiply result by the base
     
     # Return the final result
+
+def exponent(base, exp):
+    result = 1
+    for i in range(exp):
+        result *= base
+    print(f'{base} raised to the power of {exp} is: {result}')
+
+exponent(2, 5) # 2 raised to the power of 5 is: 32
+exponent(5, 4) # 5 raised to the power of 4 is: 625
